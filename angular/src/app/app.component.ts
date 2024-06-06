af0b04f7168bc5b075b12f46eab1890dc33a76b9
Simon Porritt <simon@jsplumbtoolkit.com>
2024-06-06 19:52:22 +1000

    6.24.0

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,8 +7,7 @@ import {BrowserUIAngular, jsPlumbSurfaceComponent} from "@jsplumbtoolkit/browser
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements AfterViewInit {
 
